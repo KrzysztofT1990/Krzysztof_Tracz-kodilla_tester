274fac00e5c78240663f4f6a3993b1ffa574186f
Krzysztof Tracz <KrzysztofT1990@gmail.com>
2021-02-27 23:46:27 +0100

    0.7.Typy danych - Zadanie Hello World!

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        String welcome = "Hello World!";
+        String name = "Krzysiek";
+        int age = 30;
+
+        System.out.println(welcome + "\n" + name + "\n" + age + "\n" + name.charAt(0));
+    }
+}
