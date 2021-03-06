4097d2530cd42c1a9a4ea78fa25812dc8f32636d
Krzysztof Tracz <KrzysztofT1990@gmail.com>
2021-03-06 12:14:54 +0100

    0.11.Instrukcje warunkowe - Zadanie: rok przestępny

--- kodilla-intro/src/main/java/LeapYear.java
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class LeapYear {
+public class LeapYear {
+    public static void main(String[] args) {
+
+        int year = 1904;
+        checkYear(year);
+    }
+
+    public static void checkYear(int year) {
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 4 == 0 && year % 100 == 0 && year % 400 == 0))
+            System.out.println("Rok: " + year + " jest rokiem przestępnym.");
+        else {
+            System.out.println(year + " nie jest rokiem przestępny.");
+        }
+    }
 }
