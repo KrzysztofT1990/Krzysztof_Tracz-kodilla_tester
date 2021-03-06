b21dea28ae16a287a2b645349961d3e4688674d4
Krzysztof Tracz <KrzysztofT1990@gmail.com>
2021-03-06 12:12:42 +0100

    0.8.Zmienne - Zadanie deklarowanie i wywoływanie zmiennych

--- kodilla-intro/src/main/java/MainExtended.java
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class MainExtended {
+public class MainExtended {
+    public static void main(String[] args) {
+        String dogName = "Azor";
+        int dogAge = 4;
+        double dogWeight = 6.78d;
+        float dogHeight = 1.40f;
+        char dogGender = 'F';
+        boolean dogAfterTrainings = true;
+
+        System.out.println(
+                "Imię: " + dogName
+                        + "\nWiek: " + dogAge
+                        + "\nWaga: " + dogWeight + "kg"
+                        + "\nWysokość: " + dogHeight + "m"
+                        + "\nPłeć: " + dogGender
+                        + "\nSzkolenie: " + dogAfterTrainings
+        );
+    }
 }
