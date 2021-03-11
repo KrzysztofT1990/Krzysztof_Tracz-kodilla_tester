305575fd348b6977f2118f68d5fc7f52697f1a75
Krzysztof Tracz <KrzysztofT1990@gmail.com>
2021-03-11 23:56:34 +0100

    2.3 Ćwiczenia: instrukcje warunkowe

--- kodilla-2.Obiektowosc_w_Javie/src/main/java/FirstClass.java
+++ kodilla-2.Obiektowosc_w_Javie/src/main/java/FirstClass.java
@@ -1,2 +1,24 @@
 public class FirstClass {
+    public static void main(String[] args) {
+
+        Notebook notebook = new Notebook("900g", 499, 2021);
+        System.out.println(notebook.weight + " " + notebook.price + "złoty" + " " + notebook.year + "r.");
+        notebook.checkPrice();
+        notebook.checkWeight();
+        notebook.checkYearPrice();
+        System.out.println("=========================");
+
+        Notebook hevyNotebook = new Notebook("3000g", 850, 1999);
+        System.out.println(hevyNotebook.weight + " " + hevyNotebook.price + "złoty" + " " + hevyNotebook.year + "r.");
+        hevyNotebook.checkPrice();
+        hevyNotebook.checkWeight();
+        hevyNotebook.checkYearPrice();
+        System.out.println("=========================");
+
+        Notebook oldNotebook = new Notebook("1500g", 1500, 1995);
+        System.out.println(oldNotebook.weight + " " + oldNotebook.price + "złoty" + " " + oldNotebook.year + "r.");
+        oldNotebook.checkPrice();
+        oldNotebook.checkWeight();
+        oldNotebook.checkYearPrice();
+    }
 }
--- kodilla-2.Obiektowosc_w_Javie/src/main/java/Notebook.java
+++ kodilla-2.Obiektowosc_w_Javie/src/main/java/Notebook.java
@@ -1,2 +1,49 @@
-package PACKAGE_NAME;public class Notebook {
+public class Notebook {
+    String weight;
+    int price;
+    int year;
+
+    public Notebook(String weight, int price, int year) {
+        this.weight = weight;
+        this.price = price;
+        this.year = year;
+    }
+
+    public void checkPrice() {
+        if (this.price < 600) {
+            System.out.println("This notebook is very cheap");
+        } else if (this.price >= 600 && this.price <= 1000) {
+            System.out.println("The price is good.");
+        } else {
+            System.out.println("This notebook is expensive.");
+        }
+    }
+
+    public void checkWeight() {
+        switch (this.weight) {
+            case "900g":
+                System.out.println("Dane urządzenie jest lekkie");
+                break;
+            case "1500g":
+                System.out.println("Niezbyt ciężkie");
+                break;
+            case "3000g":
+                System.out.println("Bardzo ciężkie");
+                break;
+            default:
+                System.out.println("Brak danych o wadze");
+                break;
+        }
+    }
+
+    public void checkYearPrice() {
+        if (this.price < 1000 && this.year >= 2000) {
+            System.out.println("Dobra oferta !!");
+        } else if (this.price >= 1000 && this.year < 2000) {
+            System.out.println("Zła oferta");
+        } else {
+            System.out.println("Brak komunikatu");
+        }
+    }
+
 }
