08275f6b63b75d5a6268288b02c8a72d2f77ffdf
Krzysztof Tracz <KrzysztofT1990@gmail.com>
2021-04-06 17:04:59 +0200

    5.4. Zaawansowane zagadnienia dotyczące niemutowalności - Zadanie: księgozbiór

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookApplication.java
@@ -0,0 +1,14 @@
+package com.kodilla.collections.adv.immutable.special.homework;
+
+public class BookApplication {
+
+    public static void main(String[] args) {
+
+        Book book1 = BookManager.createBook("cos1", "cos2");
+        Book book2 = BookManager.createBook("cos", "cos");
+
+        System.out.println(book1 == book2);
+        System.out.println(book1.equals(book2));
+
+    }
+}
---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookManager.java
@@ -0,0 +1,9 @@
+package com.kodilla.collections.adv.immutable.special.homework;
+
+public class BookManager  {
+
+    public static Book createBook(String title, String author) {
+       return new Book (title,author);
+
+    }
+}
