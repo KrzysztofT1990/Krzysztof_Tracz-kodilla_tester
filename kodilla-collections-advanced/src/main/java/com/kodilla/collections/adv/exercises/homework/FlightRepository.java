77ba8d6a46ac7c96dfbde9a904b9280f9ba95524
Krzysztof Tracz <KrzysztofT1990@gmail.com>
2021-04-10 22:14:32 +0200

    5.5. Przykład testów z użyciem kolekcji - Zadanie: lista lotów

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/exercises/homework/FlightRepository.java
@@ -0,0 +1,17 @@
+package com.kodilla.collections.adv.exercises.homework;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FlightRepository {
+
+    Map<String, List<Flight>> flightRepository = new HashMap();
+
+    static List<Flight> getFlightsTable() {
+        List<Flight> result = new ArrayList<>();
+
+        return result;
+    }
+}
