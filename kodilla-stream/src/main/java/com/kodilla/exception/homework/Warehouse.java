0968571de246b615537c7fb45ffdb289ccbf383f
Krzysztof Tracz <KrzysztofT1990@gmail.com>
2021-04-13 20:39:08 +0200

    6.4. Wyjątki - Zadanie: obsługa błędów

---
+++ kodilla-stream/src/main/java/com/kodilla/exception/homework/Warehouse.java
@@ -0,0 +1,23 @@
+package com.kodilla.exception.homework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Warehouse {
+
+    List<Order> orderList = new ArrayList<>();
+
+    public void addOrder(Order order) {
+        orderList.add(order);
+    }
+
+    public Order getOrder(String number) throws OrderDoesntExistException {
+        Order order = orderList
+                .stream()
+                .filter(u -> u.getNumber().equals(number))
+                .findFirst()
+                .orElseThrow(OrderDoesntExistException::new);
+        return order;
+    }
+
+}
