44cf2af8d1d7f7df6af702c3bb63996fee953ddf
Krzysztof Tracz <KrzysztofT1990@gmail.com>
2021-04-18 17:19:18 +0200

    7.2. Pobieranie danych z zewnątrz - Zadanie: maszyna do gier

---
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuit.java
@@ -0,0 +1,45 @@
+package com.kodilla.parametrized_tests.homework;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GamblingMachineTestSuit {
+
+    GamblingMachine gamblingMachine = new GamblingMachine();
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/invalidSetOfNumbers.csv", numLinesToSkip = 1)
+    public void shouldThrowExceptionWithThisSetNumber(int num1, int num2, int num3, int num4, int num5, int num6) {
+        //give
+        Set<Integer> test = new HashSet<>();
+        test.add(num1);
+        test.add(num2);
+        test.add(num3);
+        test.add(num4);
+        test.add(num5);
+        test.add(num6);
+        //when
+        //than
+        assertThrows(InvalidNumbersException.class, () -> gamblingMachine.howManyWins(test));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/invalidSetOfNumbersBecauseToShort.csv", numLinesToSkip = 1)
+    public void shouldThrowExceptionWithThisSetNumber(int num1, int num2, int num3, int num4) {
+        //give
+        Set<Integer> test = new HashSet<>();
+        test.add(num1);
+        test.add(num2);
+        test.add(num3);
+        test.add(num4);
+        //when
+        //than
+        assertThrows(InvalidNumbersException.class, () -> gamblingMachine.howManyWins(test));
+    }
+
+}
