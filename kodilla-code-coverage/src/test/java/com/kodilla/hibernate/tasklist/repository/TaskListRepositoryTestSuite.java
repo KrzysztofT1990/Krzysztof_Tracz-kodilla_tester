989e504884e5a3dfda8d07094c40637e16d57f64
Krzysztof Tracz <KrzysztofT1990@gmail.com>
2021-06-14 23:56:15 +0200

    15.4. Testy integracyjne z bazą danych – podstawy Hibernate'a - Zadanie: tworzenie list zadań

---
+++ kodilla-code-coverage/src/test/java/com/kodilla/hibernate/tasklist/repository/TaskListRepositoryTestSuite.java
@@ -0,0 +1,45 @@
+package com.kodilla.hibernate.tasklist.repository;
+
+import com.kodilla.hibernate.tasklist.TaskList;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+
+public class TaskListRepositoryTestSuite {
+    @Autowired
+    private TaskListRepository taskListRepository;
+    private String listName = "First List";
+    private String description = "Here find full description";
+
+    @Test
+    public void testFindByListName() {
+        List<TaskList> readTasksList = null;
+
+        try {
+            //Given
+            TaskList taskList = new TaskList(listName, description);
+            taskListRepository.save(taskList);
+            listName = taskList.getListName();
+
+            //When
+            readTasksList = taskListRepository.findByListName(listName);
+
+            //Then
+            Assert.assertEquals(1, readTasksList.size());
+
+        } finally {
+
+            //CleanUp
+            int id = readTasksList.get(0).getId();
+            taskListRepository.deleteById(id);
+        }
+    }
+}
